d369120c0fc574c0449db1de12164131785ccf90
Thounny <thounnykeo@gmail.com>
2024-10-15 10:34:40 -0400

    Created more_functions.js | first exercise problem

---
+++ Functions/more_functions.js
@@ -0,0 +1,8 @@
+// FIRST
+function displayMailingLabel(name, address, city, state, zip) {
+  console.log(name);
+  console.log(address);
+  console.log(`${city}, ${state}, ${zip}`);
+}
+
+displayMailingLabel("Thounny", "123 abc street", "Atlanta", "GA", "12345");
