fe6be2d5321828ce6c1e8225855c61099e72b57b
Thounny <thounnykeo@gmail.com>
2024-10-15 10:25:45 -0400

    Created simple_functions from workbook

---
+++ Functions/simple_functions.js
@@ -0,0 +1,24 @@
+// define three functions
+
+// FIRST
+function favoriteThings() {
+  console.log("Name: Thounny Keo");
+  console.log("Favorite Movie: I Saw The Devil");
+  console.log("Favorite Band: King Krule");
+}
+
+// SECOND
+function whyImHere() {
+  console.log(
+    "I joined 'Learning to Code' to further educate myself on the basics."
+  );
+}
+
+// THIRD
+function favoritePlace() {
+  console.log("Chilis because I love going there with my wife lol.");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
