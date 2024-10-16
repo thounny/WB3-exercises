f04aea471ca89ba6b390ee36f1c02211943da7a6
Thounny <thounnykeo@gmail.com>
2024-10-15 21:43:40 -0400

    date conversions

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,7 @@
+const birthday = new Date("01/02/1999");
+const momBirthday = new Date("Jan 01 1979");
+const dadBirthday = new Date(1964, 15, 10);
+
+console.log(birthday.toLocaleString());
+console.log(momBirthday.toLocaleString());
+console.log(dadBirthday.toLocaleString());
